7d1b84bea8f337303e6b1f14167dc70be2b05408
Nico Maker <nicola.marano02@gmail.com>
2024-01-18 15:32:31 +0100

    MQTT

--- Lettura_dati_Raspberry/Lettura_dati_Raspberry/obj/Debug/net7.0/Lettura_dati_Raspberry.AssemblyInfo.cs
+++ Lettura_dati_Raspberry/Lettura_dati_Raspberry/obj/Debug/net7.0/Lettura_dati_Raspberry.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lettura_dati_Raspberry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7201c2caad715b4d655167f9b9c3e47d025034")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lettura_dati_Raspberry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lettura_dati_Raspberry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
