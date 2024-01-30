efc35099d24ab224f379e29c0e618738c216e473
Nico Maker <nicola.marano02@gmail.com>
2024-01-30 10:19:17 +0100

    Aggiunta messaggi della ram rom e cpu nel topic ogni minuto

--- Lettura_dati_Raspberry/Lettura_dati_Raspberry/Program.cs
+++ Lettura_dati_Raspberry/Lettura_dati_Raspberry/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Lettura_dati_Raspberry;
 using System;
+using System.Runtime.Intrinsics.Arm;
 namespace lettura_dati_Raspberry;
 class Program
 {
@@ -7,23 +8,29 @@ class Program
     {
         Data data = new Data();
         
-        List <SensorData> Ram = new List <SensorData>();
-        List <SensorData> Rom = new List <SensorData>();
-        List <SensorData> Cpu = new List <SensorData>();
 
-        Ram = data.GetRamInfo();
-        Rom = data.GetRomInfo();
-        Cpu = data.GetCpuInfo(); 
+        var timer = new Timer(async _ =>
+        {
+            await DateperMinute(data);
+        }, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
 
+        Console.WriteLine("Press Enter to exit.");
+        Console.ReadLine();
+    }
+
+    static async Task DateperMinute(Data data)
+    {
         // invia i dati via MQTT
 
-        for(int i = 0; i < Ram.Count; i++)
-            await DataSend.Send(Ram[i].Name, Ram[i].Value);
+        foreach (var ramData in data.GetRamInfo())
+            await DataSend.Send(ramData.Name, ramData.Value);
 
-        for (int i = 0; i < Rom.Count; i++)
-            await DataSend.Send(Rom[i].Name, Rom[i].Value);
+        foreach (var romData in data.GetRomInfo())
+            await DataSend.Send(romData.Name, romData.Value);
 
-        for (int i = 0; i < Cpu.Count; i++)
-            await DataSend.Send(Cpu[i].Name, Cpu[i].Value);
+        foreach (var cpuData in data.GetCpuInfo())
+            await DataSend.Send(cpuData.Name, cpuData.Value);
+
+        Console.WriteLine("Data sent to MQTT.");
     }
 }
--- Readme.md
+++ Readme.md
@@ -9,7 +9,9 @@ prima parte di configurazione degli using
 intanto parte di configurazione degli using e namespace
 
 ```C#
+using Lettura_dati_Raspberry;
 using System;
+using System.Runtime.Intrinsics.Arm;
 namespace lettura_dati_Raspberry;
 ```
 
@@ -647,6 +649,41 @@ public List<SensorData> GetCpuInfo()
     }
 
     return sensorData;
+}
+```
+
+infine mettere dati perchè arrivino ogni minuto i nuovi dati
+```C#
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Data data = new Data();
+        
+        var timer = new Timer(async _ => // va vedere il tempo di timer
+        {
+            await DateperMinute(data);
+        }, null, TimeSpan.Zero, TimeSpan.FromMinutes(1)); // manda una voolta al minuto
+
+        Console.WriteLine("Press Enter to exit.");
+        Console.ReadLine();
+    }
+
+    static async Task DateperMinute(Data data)
+    {
+        // invia i dati via MQTT
+
+        foreach (var ramData in data.GetRamInfo())
+            await DataSend.Send(ramData.Name, ramData.Value);
+
+        foreach (var romData in data.GetRomInfo())
+            await DataSend.Send(romData.Name, romData.Value);
+
+        foreach (var cpuData in data.GetCpuInfo())
+            await DataSend.Send(cpuData.Name, cpuData.Value);
+
+        Console.WriteLine("Data sent to MQTT."); // manda messaggio che ti dice se è arrivato il messaggi 
+    }
 }
 ```
 
